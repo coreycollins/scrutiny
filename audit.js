3e2a65a2606095387f2f658380fbd1a7add73c7e
Corey Collins <coreyecollins@gmail.com>
2016-05-31 10:49:06 -0500

    commented some functions

--- audit.js
+++ audit.js
@@ -1,21 +1,33 @@
 var Promise = require('bluebird')
 
 module.exports = function audit (options) {
-  // List audits
+  /**
+   *  cmd "list" retreives all audits sorted by created date.
+   *
+   *  @optional schema - schema of table space to filter by.
+   */
   this.add({role: 'audit', cmd: 'list'}, function (msg, done) {
     this.make('audits', 'audit').list$({schema: msg.schema}, (err, audits) => {
       done(err, audits)
     })
   })
 
-  // Get audit
+  /**
+   *  cmd "get" retreives the audit by  id
+   *
+   *  @required id - id of the  audit.
+   */
   this.add({role: 'audit', cmd: 'get'}, function (msg, done) {
     this.make('audits', 'audit').load$(msg.id, (err, audits) => {
       done(err, audits)
     })
   })
 
-  // Get audit by job id
+  /**
+   *  cmd "getByJob" retreives the audit by job id
+   *
+   *  @required job_id - job id of the  audit.
+   */
   this.add({role: 'audit', cmd: 'getByJob'}, function (msg, done) {
     this.make('audits', 'audit').list$({job_id: msg.job_id}, (err, audits) => {
       if (audits.length == 0) {
@@ -25,7 +37,17 @@ module.exports = function audit (options) {
     })
   })
 
-  // Create audit
+  /**
+   *  cmd "create" creates an audit.
+   *
+   *  @required job_id - job id of the new audit. Should exist in table.
+   *  @required table - Name of staging table in staging database
+
+   *  @options - {
+   *    name: "Name of the audit",
+   *    auidtor: <user>
+   *  }
+   */
   this.add({role: 'audit', cmd: 'create'}, function (msg, done) {
     var options = msg.options || {}
 
@@ -37,8 +59,6 @@ module.exports = function audit (options) {
 
     // TODO: search for job_id in staging table.
     //       throw error if doesn't exist
-
-
 
     // Build audit
     var audit = {
@@ -53,7 +73,10 @@ module.exports = function audit (options) {
     })
   })
 
-  // Submit audit
+  /**
+   *  cmd "submit" sets the audit status to 'submitted' to prepare the
+   *  audit for QA by the auditor.
+   */
   this.add({role: 'audit', cmd: 'submit'}, function (msg, done) {
     var auditEntity = this.make('audits', 'audit')
 
